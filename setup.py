bbb128c6e4a2adb18c77fd7ec65551f1429b2e57
Celeste Griffin <celestebgriff@gmail.com>
2020-02-11 12:16:57 -0500

    update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="lambdata-celesteg",
-    version="0.5",
+    version="0.3",
     author="Celeste Griffin",
     author_email="celestebgriff@gmail.com",
     description="helper functions",
